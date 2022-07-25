45744d08dcd3c5f8d58d58c3db5a9462f56377b3
Karan Singare <karansingare@gmail.com>
2022-07-25 12:32:31 +0530

    shortcut service added to export

--- lib/drag-to-select.module.d.ts
+++ lib/drag-to-select.module.d.ts
@@ -4,9 +4,10 @@ import * as i0 from "@angular/core";
 import * as i1 from "./select-container.component";
 import * as i2 from "./select-item.directive";
 import * as i3 from "@angular/common";
+import * as i4 from "./shortcut.service";
 export declare class DragToSelectModule {
     static forRoot(config?: Partial<DragToSelectConfig>): ModuleWithProviders<DragToSelectModule>;
     static ɵfac: i0.ɵɵFactoryDeclaration<DragToSelectModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<DragToSelectModule, [typeof i1.SelectContainerComponent, typeof i2.SelectItemDirective], [typeof i3.CommonModule], [typeof i1.SelectContainerComponent, typeof i2.SelectItemDirective]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<DragToSelectModule, [typeof i1.SelectContainerComponent, typeof i2.SelectItemDirective], [typeof i3.CommonModule], [typeof i1.SelectContainerComponent, typeof i2.SelectItemDirective, typeof i4.ShortcutService]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<DragToSelectModule>;
 }
